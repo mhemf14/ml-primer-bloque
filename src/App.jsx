0b30ca61e8a1a713db1505a12f8b819407dc073c
mhemf14 <51766278+mhemf14@users.noreply.github.com>
2025-12-17 12:36:00 -0300

    Update App and simplify Vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { Client } from 'boardgame.io/react';
-import { MLPrimerBloque } from './game/ml/game';
-
-function Board({ G, ctx, moves, playerID }) {
-  const p = G.players[playerID];
-  return (
-    <div>
-      <h3>Jugador {playerID}</h3>
-      <div>Mazo: {p.castillo.length}</div>
-      <div>Mano: {p.mano.map(c =>
-        <button key={c} onClick={() => {
-          if (c === 'ORO_BASICO') moves.playOro(c);
-          else moves.playAliado(c);
-        }}>{c}</button>
-      )}</div>
-      <div>Defensa: {p.lineaDefensa.map(a =>
-        <button key={a.instanceId} onClick={() => moves.declareAttack(a.instanceId)}>
-          Atacar {a.cardId}
-        </button>
-      )}</div>
-      <button onClick={moves.assignDamage}>Asignar Daño</button>
-      <button onClick={moves.endTurn}>Fin Turno</button>
-    </div>
-  );
-}
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Client } from 'boardgame.io/client'
+import { MLPrimerBloque } from './game/ml/game'
 
 const GameClient = Client({
   game: MLPrimerBloque,
-  board: Board,
   numPlayers: 2,
-});
+})
 
-export default function App() {
-  return <GameClient playerID="0" />;
+function App() {
+  const [state, setState] = React.useState(null)
+
+  React.useEffect(() => {
+    GameClient.start()
+    GameClient.subscribe(setState)
+    return () => GameClient.stop()
+  }, [])
+
+  if (!state) return <div>Cargando...</div>
+
+  const { G, ctx } = state
+
+  return (
+    <div>
+      <h3>Jugador {ctx.currentPlayer}</h3>
+      <div>Cartas en Castillo: {G.players['0'].castillo.length}</div>
+      <button onClick={() => GameClient.moves.endTurn()}>
+        Terminar Turno
+      </button>
+    </div>
+  )
 }
+
+export default App
